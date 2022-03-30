5278205ed0c11547d4fdca0a9ea9e1932c98dbda
Edoardo <edoardogamurrini@gmail.com>
2022-03-30 18:08:54 +0200

    Co-authored-by: JurgensM <JurgensM@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+let box = document.getElementById('box'),
+    btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+  box.classList.toggle('hidden');
+}, false);
