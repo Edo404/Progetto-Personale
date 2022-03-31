e7c69e836a6675ba565b38bad4b04ae2798f7cc9
Edoardo <edoardogamurrini@gmail.com>
2022-03-31 13:12:18 +0200

    navbar funzionante ma da invertire quando si vede e quando no

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 
 function myFunction(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
 }
 
 
-let box = document.getElementById('box'),
-    btn = document.querySelector('button');
+
+let btn = document.querySelector('.hamburger');
+let box = document.querySelectorAll('.item'), i;
 
 btn.addEventListener('click', function () {
-  box.classList.toggle('hidden');
+  for(i = 0; i < box.length; ++i){
+    box[i].classList.toggle('nascondi');
+  }
+  
+
 }, false);
+
+// let box = document.getElementById('box'),
+//     btn = document.querySelector('button');
+
+// btn.addEventListener('click', function () {
+//   box.classList.toggle('hidden');
+// }, false);
