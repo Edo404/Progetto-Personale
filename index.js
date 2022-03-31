1803bc0e112b7bb7d5239d430970192d1a673557
Edoardo <edoardogamurrini@gmail.com>
2022-03-31 16:20:52 +0200

    tutto funzionante

--- index.js
+++ index.js
@@ -10,15 +10,9 @@ let box = document.querySelectorAll('.item'), i;
 
 btn.addEventListener('click', function () {
   for(i = 0; i < box.length; ++i){
-    box[i].classList.toggle('nascondi');
+    box[i].classList.toggle('show');
   }
   
 
 }, false);
 
-// let box = document.getElementById('box'),
-//     btn = document.querySelector('button');
-
-// btn.addEventListener('click', function () {
-//   box.classList.toggle('hidden');
-// }, false);
