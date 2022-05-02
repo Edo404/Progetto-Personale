d513a92eaaf400dcf7f067c1ee4f327d4523374b
Edoardo <edoardogamurrini@gmail.com>
2022-05-02 14:52:53 +0200

    codice commentato

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
+// preloader
 $(document).ready(function() {
-  
   setTimeout(function() {
     $('#ctn-preloader').addClass('loaded');
-    // al termine viene visualizzato lo scroll
+    // scroll visualized at the end
     $('body').removeClass('no-scroll-y');
 
     if ($('#ctn-preloader').hasClass('loaded')) {
-      // intera sezione del preloader viene eliminata 
+      // whole preloader section is deleted
       $('#preloader').delay(1000).queue(function() {
         $(this).remove();
       });
     }
   }, 1000);
-  
 });
 
+// change logo color when the navbar is black
 window.addEventListener("scroll", (event) => {
   let scrollY = this.scrollY;
   
   if (scrollY >= 100){
-      
-      $("#logo").css({
-        "display": "none",
-        });
-      $("#logo-white").css({
-          "display": "inline",
-      });
-      
- 
-  }  else if (scrollY < 100 || scrollY == 0){
-    $("#logo-white").css({
+    $("#logo").css({
       "display": "none",
+    });
+    $("#logo-white").css({
+      "display": "inline",
+    });
+  } else if (scrollY < 100 || scrollY == 0){
+      $("#logo-white").css({
+        "display": "none",
       });
       $("#logo").css({
         "display": "inline",
-        });
-  }
-  
-
+      });
+    }
 });
 
-
-
+// transition of the hamburger button
 function myFunction(x) {
   x.classList.toggle("change");
 }
@@ -55,22 +49,18 @@ btn.addEventListener('click', function () {
   }
 }, false);
 
+// navbar background is black if scroll is > 100
 $(window).scroll(function() {    
   var scroll = $(window).scrollTop();
 
   if (scroll >= 100) {
-      $(".container").addClass("scrolling");
+    $(".container").addClass("scrolling");
   } else {
       $(".container").removeClass("scrolling");
-  }
-
-    // var img = document.createElement("img");
-    //   img.src = 'images/logo_bianco.png';
-    //   var parent = document.querySelector(".logo");
-    //   parent.appendChild(img);
-
+    }
 });
 
+// year counter in footer 2
 const year = document.getElementById("actualYear");
 const actualYear = new Date();
 year.innerHTML = actualYear.getFullYear();
